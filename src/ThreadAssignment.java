8c7356587a866a2cb895c61c42127c76e2bef80f
sheima-ayadi <80485125+sheima-ayadi@users.noreply.github.com>
2021-11-28 18:22:36 +0100

    Update ThreadAssignment.java

--- src/ThreadAssignment.java
+++ src/ThreadAssignment.java
@@ -2,7 +2,9 @@ public class ThreadAssignment {
 
     static class Counter {
         void count() {
-            //todo implement me
+            for (int i=350;i>0;i--) //Implement the count method
+                System.out.println(i);
+            System.out.println("finish!");
         }
     }
 
@@ -15,14 +17,35 @@ public class ThreadAssignment {
 
         @Override
         public void run() {
-            counter.count();
+            synchronized (counter){   //Synchronized
+                counter.count();
+            }
         }
     }
 
     public static void main(String[] args) {
         Counter counter = new Counter();
 
-        new MyThread(counter).start();
-        new MyThread(counter).start();
+        MyThread t1 = new MyThread(counter);
+        MyThread t2 = new MyThread(counter);
+        
+        //Manage race condition so that the counting doesn't overlap
+        t1.start();
+        try{
+            t1.join();
+        }catch(InterruptedException e ){
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+
+        t2.start();
+        try{
+            t2.join();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        //Print 'Done !' when both threads terminate
+        System.out.println("Done!");
     }
 }
